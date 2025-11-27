86a6abcee326dd6f0d959853eadef09b177156d1
laxmansingh-rajput <laxmanprograming@gmail.com>
2025-11-27 10:56:51 +0530

    20 % fixing

--- frontend/src/components/workSpace.jsx
+++ frontend/src/components/workSpace.jsx
@@ -18,7 +18,8 @@ const WorkSpace = () => {
     }
 
     return (
-        <div className='min-h-[100vh] w-[100vw] bg-[#F4F9FF] text-xl text-[#1A1A1A] dark:bg-primary-dark dark:text-[#A0A0B2]  '>
+        <div className='h-full w-[100vw] bg-[#F4F9FF] text-xl text-[#1A1A1A] dark:bg-primary-dark dark:text-[#A0A0B2] 
+        flex items-center justify-center   '>
             <ComponentToRender />
         </div>
     );
